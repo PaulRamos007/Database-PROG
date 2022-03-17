7f7c89d5ac057b76a952a812275d1271e0c9f239
PaulRamos007 <pramos01@mynbcc.ca>
2022-03-17 18:51:30 -0300

    DB Project

---
+++ TechSupportWeb/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TechSupportWeb.Models;
+
+namespace TechSupportWeb.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index(int id)
+        {
+            var context = new TechSupportEntities();
+
+            Customer customers = context.Customers.FirstOrDefault(x => x.CustomerID == id);
+
+            return View(customers);
+        }
+
+        public ActionResult CustomersList(string searchTerm)
+        {
+            var context = new TechSupportEntities();
+
+            List<Customer> customers = context.Customers.ToList();
+
+            if (!string.IsNullOrWhiteSpace(searchTerm))
+            {
+                customers = customers.Where(c => c.Name.IndexOf(searchTerm) != -1).ToList();
+            }
+
+            return View(customers);
+        }
+    }
+}
---
+++ TechSupportWeb/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TechSupportWeb.Models;
+
+namespace TechSupportWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index(string id)
+        {
+            var context = new TechSupportEntities();
+
+            Product products = context.Products.FirstOrDefault(x => x.ProductCode == id);
+
+            return View(products);
+        }
+
+        public ActionResult ProductList()
+        {
+            var context = new TechSupportEntities();
+
+            List<Product> products = context.Products.ToList();
+
+            return View(products);
+        }
+    }
+}
